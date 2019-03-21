e44ba44b655ab30a3ac5abd7646e4b8544c51581
Izabela <izabela.nowosada@gmail.com>
2019-03-21 15:25:03 +0100

    Add content and fix height

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,35 +1,59 @@
 <template>
   <q-page class="flex column q-ma-lg">
     <h3>Featured</h3>
-    <q-page class="flex row justify-between">
+    <q-page class="flex row justify-between items-start">
     <q-card inline style="width: 500px">
-      <q-card-main class="flex row items-center">
-      <q-card-media>
-        <img src="../../logoKira.jpg" style="width: 60px">
+      <q-card-main class="flex row items-center" style="height: 160px">
+      <q-card-media class="col-3">
+        <img src="../../logoKira.jpg" style="height: 100%">
       </q-card-media>
-      <q-card-title>Kira
-         <span slot="subtitle" class="text-faded">It's How You Know What's in Your Contract</span>
+      <q-card-title class="col-7">Kira
+         <p class="text-faded">It's How You Know What's in Your Contract</p>
       </q-card-title>
       <div>
-      <q-checkbox v-model="checked"/>
+      <q-checkbox class="col-2" v-model="checked"/>
       </div>
       </q-card-main>
       <q-card-separator/>
-      <q-card-main>
-        Kira is a powerful machine learning software that identifies, extracts, and analyzes text in your contracts and other documents.
+      <q-card-main style="height: 200px">
+        <big class="text-right">Kira is a powerful machine learning software that identifies, extracts, and analyzes text in your contracts and other documents.</big>
       </q-card-main>
     </q-card>
 
     <q-card inline style="width: 500px">
-      <q-card-title>Doxley</q-card-title>
+      <q-card-main class="flex row items-center" style="height: 160px">
+      <q-card-media class="col-3">
+        <img src="../../logoDoxly.png" style="height: 100%">
+      </q-card-media>
+      <q-card-title class="col-7">Doxly
+         <p class="text-faded">Secure Portal for End to End Transactions</p>
+      </q-card-title>
+      <div>
+      <q-checkbox class="col-2" v-model="checked"/>
+      </div>
+      </q-card-main>
       <q-card-separator/>
-      <q-card-main>Kira is a powerful machine learning software that identifies, extracts, and analyzes text in your contracts and other documents.</q-card-main>
+      <q-card-main style="height: 200px">
+        <big class="text-right">Doxly’s secure transaction management platform brings control, peace of mind, and velocity to legal transactions.</big>
+      </q-card-main>
     </q-card>
 
     <q-card inline style="width: 500px">
-      <q-card-title>Clause</q-card-title>
+      <q-card-main class="flex row items-center" style="height: 160px">
+      <q-card-media class="col-3">
+        <img src="../../logoClause.png" style="height: 100%">
+      </q-card-media>
+      <q-card-title class="col-7">Clause
+         <p class="text-faded">Connected Contracting</p>
+      </q-card-title>
+      <div>
+      <q-checkbox class="col-2" v-model="checked"/>
+      </div>
+      </q-card-main>
       <q-card-separator/>
-      <q-card-main>Kira is a powerful machine learning software that identifies, extracts, and analyzes text in your contracts and other documents.</q-card-main>
+      <q-card-main style="height: 200px">
+        <big class="text-right">Clause builds the technology infrastructure for 'connected contracts' to increase the value and improve the management of your business agreements.</big>
+      </q-card-main>
     </q-card>
 </q-page>
   </q-page>
