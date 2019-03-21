e34fe3278e502c6200ead8b1d76a456f33fa1489
Izabela <izabela.nowosada@gmail.com>
2019-03-21 14:27:58 +0100

    Add title and margin

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,9 @@
 <template>
-  <q-page class="flex flex-center justify-around">
-   <!--  <h3 class="q-display-2 text-right">Featured</h3> -->
+  <q-page class="flex column q-ma-lg">
+    <h3>Featured</h3>
+    <q-page class="flex row justify-between">
     <q-card inline style="width: 400px">
+      <q-card-main>
       <q-card-media>
         <img src="../../logoKira.jpg" style="width: 100px">
       </q-card-media>
@@ -9,6 +11,7 @@
          <span slot="subtitle" class="text-faded">It's How You Know What's in Your Contract</span>
          <q-checkbox slot="right" v-model="checked" checked-icon unchecked-icon/>
       </q-card-title>
+      </q-card-main>
       <q-card-separator/>
       <q-card-main>
         Kira is a powerful machine learning software that identifies, extracts, and analyzes text in your contracts and other documents.
@@ -16,17 +19,17 @@
     </q-card>
 
     <q-card inline style="width: 400px">
-      <q-card-title>Kira</q-card-title>
+      <q-card-title>Doxley</q-card-title>
       <q-card-separator/>
       <q-card-main>Kira is a powerful machine learning software that identifies, extracts, and analyzes text in your contracts and other documents.</q-card-main>
     </q-card>
 
     <q-card inline style="width: 400px">
-      <q-card-title>Kira</q-card-title>
+      <q-card-title>Clause</q-card-title>
       <q-card-separator/>
       <q-card-main>Kira is a powerful machine learning software that identifies, extracts, and analyzes text in your contracts and other documents.</q-card-main>
     </q-card>
-
+</q-page>
   </q-page>
 </template>
 
