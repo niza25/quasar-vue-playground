3c1ada477d31883c73eee45014247bad8b8f927c
Izabela <izabela.nowosada@gmail.com>
2019-03-21 14:50:29 +0100

    Add layout to first main card

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,15 +2,17 @@
   <q-page class="flex column q-ma-lg">
     <h3>Featured</h3>
     <q-page class="flex row justify-between">
-    <q-card inline style="width: 400px">
-      <q-card-main>
+    <q-card inline style="width: 500px">
+      <q-card-main class="flex row items-center">
       <q-card-media>
-        <img src="../../logoKira.jpg" style="width: 100px">
+        <img src="../../logoKira.jpg" style="width: 60px">
       </q-card-media>
       <q-card-title>Kira
          <span slot="subtitle" class="text-faded">It's How You Know What's in Your Contract</span>
-         <q-checkbox slot="right" v-model="checked" checked-icon unchecked-icon/>
       </q-card-title>
+      <div>
+      <q-checkbox v-model="checked"/>
+      </div>
       </q-card-main>
       <q-card-separator/>
       <q-card-main>
@@ -18,13 +20,13 @@
       </q-card-main>
     </q-card>
 
-    <q-card inline style="width: 400px">
+    <q-card inline style="width: 500px">
       <q-card-title>Doxley</q-card-title>
       <q-card-separator/>
       <q-card-main>Kira is a powerful machine learning software that identifies, extracts, and analyzes text in your contracts and other documents.</q-card-main>
     </q-card>
 
-    <q-card inline style="width: 400px">
+    <q-card inline style="width: 500px">
       <q-card-title>Clause</q-card-title>
       <q-card-separator/>
       <q-card-main>Kira is a powerful machine learning software that identifies, extracts, and analyzes text in your contracts and other documents.</q-card-main>
