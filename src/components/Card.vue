d2ffb6ed95717808ddadc498a6f25b77c9169274
Izabela <izabela.nowosada@gmail.com>
2019-03-27 12:22:58 +0100

    Fix spacing checkbox

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,62 +1,21 @@
 <template>
-<q-card inline class="q-ma-sm">
-      <q-card-media>
-        <img :src="card.logo">
-      </q-card-media>
-      <q-list>
-        <q-item>
-          <q-item-side>
-            <q-item-tile color="primary" icon="local bar" />
-          </q-item-side>
-          <q-item-main>
-            <q-item-tile label>Bar XYZ</q-item-tile>
-            <q-item-tile sublabel>Have a drink.</q-item-tile>
-          </q-item-main>
-        </q-item>
-        <q-item>
-          <q-item-side>
-            <q-item-tile color="red" icon="local gas station" />
-          </q-item-side>
-          <q-item-main>
-            <q-item-tile label>Gas Station</q-item-tile>
-            <q-item-tile sublabel>Fill your gas tank.</q-item-tile>
-          </q-item-main>
-        </q-item>
-        <q-item>
-          <q-item-side>
-            <q-item-tile color="amber" icon="local movies" />
-          </q-item-side>
-          <q-item-main>
-            <q-item-tile label>Cinema XYZ</q-item-tile>
-            <q-item-tile sublabel>Watch a movie.</q-item-tile>
-          </q-item-main>
-        </q-item>
-      </q-list>
-    </q-card>
-  <!-- <q-card inline style="max-width: 530px">
+  <q-card inline style="max-width: 530px">
       <q-item>
       <q-item-side>
-        <q-item-tile color="primary">
-          <img src={{card.logo}}/>
-        </q-item-tile>
+        <img :src="card.logo" style="width: 70px">
       </q-item-side>
-      <q-item-main>
+       <q-item-main class="col">
         <q-item-tile label>{{card.title}}</q-item-tile>
-        <q-item-tile sublabel>{{card.subtitle}}</q-item-tile>
+        <q-item-tile sublabel class="text-faded">{{card.subtitle}}</q-item-tile>
       </q-item-main>
-    </q-item>
-      <q-item>
-        <q-checkbox class="col-1" v-on:change="toggleCheckbox"/>
+      <q-checkbox v-model="card.checked" v-on:change="toggleCheckbox"/>
       </q-item>
-    <q-card-separator/>
+       <q-card-separator/>
     <q-card-main>
       <h6 class='text-left'>{{card.description}}</h6>
     </q-card-main>
-  </q-card> -->
+  </q-card>
 </template>
-
-<style scoped>
-</style>
 
 <script>
 export default {
